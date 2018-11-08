83f0d84553c0883e6cedd0901538adeed0001a54
davidbirch <david.birch@imperial.ac.uk>
2018-11-08 18:09:34 +0000

    Support for Zip file uploads

--- packages/ove-asset-manager/src/OVE.Service.AssetManager/Domain/S3AssetFileOperations.cs
+++ packages/ove-asset-manager/src/OVE.Service.AssetManager/Domain/S3AssetFileOperations.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -12,6 +11,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using OVE.Service.AssetManager.Models;
+using System.IO.Compression;
 
 namespace OVE.Service.AssetManager.Domain {
     public class S3AssetFileOperations : IAssetFileOperations {
@@ -22,27 +22,27 @@ namespace OVE.Service.AssetManager.Domain {
         private const string S3ClientAccessKey = "s3Client:AccessKey";
         private const string S3ClientSecret = "s3Client:Secret";
         private const string S3ClientServiceUrl = "s3Client:ServiceURL";
-        
+
         public S3AssetFileOperations(ILogger<S3AssetFileOperations> logger, IConfiguration configuration) {
             _logger = logger;
             _configuration = configuration;
         }
-        
+
         private IAmazonS3 GetS3Client(IConfiguration configuration) {
-            
+
             IAmazonS3 s3Client = new AmazonS3Client(
                 configuration.GetValue<string>(S3ClientAccessKey),
                 configuration.GetValue<string>(S3ClientSecret),
                 new AmazonS3Config {
                     ServiceURL = configuration.GetValue<string>(S3ClientServiceUrl),
-                    UseHttp = true, 
+                    UseHttp = true,
                     ForcePathStyle = true
                 }
             );
             _logger.LogInformation("Created new S3 Client");
             return s3Client;
         }
-        
+
         #region Implementation of IFileOperations
 
         public string ResolveFileUrl(OVEAssetModel asset) {
@@ -60,11 +60,11 @@ namespace OVE.Service.AssetManager.Domain {
         }
 
         public async Task<bool> Delete(OVEAssetModel asset) {
-            _logger.LogInformation("about to delete file "+asset);
+            _logger.LogInformation("about to delete file " + asset);
             try {
                 using (var s3Client = GetS3Client(_configuration)) {
                     // delete folder containing the file and everything
-                    
+
                     ListObjectsResponse files = null;
                     while (files == null || files.S3Objects.Any()) {
                         if (files != null && files.S3Objects.Any()) {
@@ -98,53 +98,105 @@ namespace OVE.Service.AssetManager.Domain {
         }
 
         public async Task<bool> Save(OVEAssetModel asset, IFormFile upload) {
-            _logger.LogInformation("about to upload "+asset);
+            _logger.LogInformation("about to upload " + asset);
 
             // set up the filename            
             var ext = ValidateExtension(Path.GetExtension(upload.FileName).ToLower());
-            asset.StorageLocation = Guid.NewGuid() +"/"+S3Sanitize(Path.GetFileNameWithoutExtension(upload.FileName),ext)+ext;
+            asset.StorageLocation = Guid.NewGuid() + "/" +
+                                    S3Sanitize(Path.GetFileNameWithoutExtension(upload.FileName), ext) + ext;
 
             try {
 
                 using (var s3Client = GetS3Client(_configuration)) {
-                    using (var file = upload.OpenReadStream()) {
 
-                        // find or create the bucket
-                        var buckets = await s3Client.ListBucketsAsync();
-                        if (buckets.Buckets.FirstOrDefault(b => b.BucketName == asset.Project) == null) {
-                            var res = await s3Client.PutBucketAsync(asset.Project);
-                            if (res.HttpStatusCode != HttpStatusCode.OK) {
-                                throw new Exception("could not create bucket" + asset.Project);
-                            }
-
-                            var openBucketPolicy =
-                                "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"*\"]},\"Action\":[\"s3:GetBucketLocation\",\"s3:ListBucket\"],\"Resource\":[\"arn:aws:s3:::" +
-                                asset.Project +
-                                "\"]},{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"*\"]},\"Action\":[\"s3:GetObject\"],\"Resource\":[\"arn:aws:s3:::" +
-                                asset.Project + "/*\"]}]}";
-
-                            await s3Client.PutBucketPolicyAsync(asset.Project, openBucketPolicy);
-
-                            _logger.LogInformation("Created bucket "+asset.Project);
+                    // find or create the bucket
+                    var buckets = await s3Client.ListBucketsAsync();
+                    if (buckets.Buckets.FirstOrDefault(b => b.BucketName == asset.Project) == null) {
+                        var res = await s3Client.PutBucketAsync(asset.Project);
+                        if (res.HttpStatusCode != HttpStatusCode.OK) {
+                            throw new Exception("could not create bucket" + asset.Project);
                         }
 
-                        // upload it 
+                        var openBucketPolicy =
+                            "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"*\"]},\"Action\":[\"s3:GetBucketLocation\",\"s3:ListBucket\"],\"Resource\":[\"arn:aws:s3:::" +
+                            asset.Project +
+                            "\"]},{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"*\"]},\"Action\":[\"s3:GetObject\"],\"Resource\":[\"arn:aws:s3:::" +
+                            asset.Project + "/*\"]}]}";
 
-                        using (var fileTransferUtility = new TransferUtility(s3Client)) {
-                            // upload the file into its own folder
-                            await fileTransferUtility.UploadAsync(file, asset.Project, asset.StorageLocation);
-                        }
+                        await s3Client.PutBucketPolicyAsync(asset.Project, openBucketPolicy);
 
+                        _logger.LogInformation("Created bucket " + asset.Project);
+                    }
+
+                    // upload the asset
+                    await Upload(s3Client, asset.Project, asset.StorageLocation, upload);
+
+                    // is it a zip file? 
+                    if (asset.StorageLocation.EndsWith(".zip")) {
+                        _logger.LogInformation("about to unzip and asset " + asset.StorageLocation);
+                        var files = await UnZipAsset(s3Client, asset, upload);
+                        _logger.LogInformation("unzipped " + files + " files to the object store" +
+                                               asset.StorageLocation);
                     }
                 }
 
-                _logger.LogInformation("uploaded "+asset);
+                _logger.LogInformation("uploaded " + asset);
                 return true;
-            } catch (Exception e) {
-                _logger.LogError(e,"Failed to upload file for "+asset);
-                return false; 
+            }
+            catch (Exception e) {
+                _logger.LogError(e, "Failed to upload file for " + asset);
+                return false;
             }
 
+        }
+
+        private async Task<int> UnZipAsset(IAmazonS3 s3Client, OVEAssetModel asset, IFormFile upload) {
+            string prefixFolder = asset.StorageLocation.Split("/").FirstOrDefault() + "/";
+
+            int filesUploaded = 0;
+
+            using (var zipFile = upload.OpenReadStream()) {
+                var archive = new ZipArchive(zipFile);
+                foreach (var entry in archive.Entries.Where(f => f.FullName.Contains("."))) {
+                    // only files
+                    var location = UnzipLocation(entry.FullName, prefixFolder);
+
+                    using (var file = entry.Open()) {
+                        //raw unzipped streams do not have length so copy to memory stream
+                        using (var ms = new MemoryStream()) {
+                            await file.CopyToAsync(ms);
+                            await Upload(s3Client, asset.Project, location, ms);
+                        }
+                    }
+
+                    filesUploaded++;
+                }
+            }
+
+            return filesUploaded;
+        }
+
+        private string UnzipLocation(string entry, string prefixFolder) {
+            var originalExt = Path.GetExtension(entry);
+            var ext = ValidateExtension(originalExt.ToLower());
+            var location = prefixFolder + S3Sanitize(entry.Replace(originalExt, ""), ext) + ext;
+            return location;
+        }
+
+        private async Task Upload(IAmazonS3 s3Client, string bucketName, string assetStorageLocation,
+            IFormFile upload) {
+            // open and upload it 
+            using (var file = upload.OpenReadStream()) {
+                await Upload(s3Client, bucketName, assetStorageLocation, file);
+            }
+        }
+
+        private async Task Upload(IAmazonS3 s3Client, string bucketName, string assetStorageLocation,
+            Stream file) {
+            using (var fileTransferUtility = new TransferUtility(s3Client)) {
+                // upload the file 
+                await fileTransferUtility.UploadAsync(file, bucketName, assetStorageLocation);
+            }
         }
 
         /// <summary>
@@ -158,7 +210,7 @@ namespace OVE.Service.AssetManager.Domain {
             Regex r = new Regex("^[a-zA-Z0-9]+$");
             input = input.Where(l => r.IsMatch(l.ToString())).Aggregate("", (acc, c) => acc + c);
 
-            return input;
+            return "." + input;
         }
 
         /// <summary>
@@ -169,14 +221,15 @@ namespace OVE.Service.AssetManager.Domain {
         /// <param name="input">input file name</param>
         /// <param name="extension">file extension</param>
         /// <returns>sanitized version</returns>
-        private string S3Sanitize(string input,string extension) {
+        private string S3Sanitize(string input, string extension) {
             try {
-                input = input.Substring(0,Math.Min( input.Length, 63 - extension.Length)); // max permitted less file extension 
+                input = input.Substring(0,
+                    Math.Min(input.Length, 63 - extension.Length)); // max permitted less file extension 
                 // ensure that slashes face the right way 
                 input = input.Replace("\\", "/");
                 // filter to valid chars 
                 Regex r = new Regex("^[-a-zA-Z0-9()_/]+$");
-                input = input.Where(l => r.IsMatch(l.ToString())).Aggregate("",(acc,c)=> acc+c);
+                input = input.Where(l => r.IsMatch(l.ToString())).Aggregate("", (acc, c) => acc + c);
 
                 //remove empty folders
                 while (input.Contains("//")) {
@@ -190,7 +243,7 @@ namespace OVE.Service.AssetManager.Domain {
 
             }
             catch (Exception e) {
-                _logger.LogError(e,"failed to sanitize s3 name "+input);
+                _logger.LogError(e, "failed to sanitize s3 name " + input);
                 return Guid.NewGuid().ToString();
             }
 
