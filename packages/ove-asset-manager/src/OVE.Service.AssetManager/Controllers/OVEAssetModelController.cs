99f624f5e49f6d154b27f4fc3c6d002167d374e3
davidbirch <david.birch@imperial.ac.uk>
2018-11-15 14:51:32 +0000

    re-enable size limit checking

--- packages/ove-asset-manager/src/OVE.Service.AssetManager/Controllers/OVEAssetModelController.cs
+++ packages/ove-asset-manager/src/OVE.Service.AssetManager/Controllers/OVEAssetModelController.cs
@@ -356,7 +356,7 @@ namespace OVE.Service.AssetManager.Controllers {
         /// <param name="upload">the asset file to upload</param>
         /// <returns></returns>
         [HttpPost]
-        [DisableRequestSizeLimit]
+        //[DisableRequestSizeLimit] enabled by request
         [Route("/OVEAssetModelController/Create/{format?}")]
         public async Task<ActionResult<OVEAssetModel>> Create(
             [Bind("Project,Name,Description,Service,AssetMeta")] OVEAssetModel oveAssetModel,
