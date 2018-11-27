4613c018b6e7b9a96a2e112ab90686d5009025ad
Ovidiu Serban <serban.ovidiu.m@gmail.com>
2018-11-27 16:33:41 +0000

    travis version + unstable

--- .travis/build-all-push.sh
+++ .travis/build-all-push.sh
@@ -4,5 +4,5 @@ scriptPath=$(dirname "$(readlink -f "$0")")
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
-${scriptPath}/../packages/ove-asset-manager/build.sh -v unstable --push && \
-${scriptPath}/../packages/ove-service-imagetiles/build.sh -v unstable --push
+${scriptPath}/../packages/ove-asset-manager/build.sh --push && \
+${scriptPath}/../packages/ove-service-imagetiles/build.sh --push
--- .travis/build-all.sh
+++ .travis/build-all.sh
@@ -2,5 +2,5 @@
 
 scriptPath=$(dirname "$(readlink -f "$0")")
 
-${scriptPath}/../packages/ove-asset-manager/build.sh -v unstable && \
-${scriptPath}/../packages/ove-service-imagetiles/build.sh -v unstable 
+${scriptPath}/../packages/ove-asset-manager/build.sh && \
+${scriptPath}/../packages/ove-service-imagetiles/build.sh
